a45e3b76912f19e3329704c0d5f72cd1447d2c35
Arjun Sharma <arjunsharma4007@gmail.com>
2026-04-30 15:53:47 +0530

    remove large files and fix gitignore

--- fetch_data.py
+++
@@ -1,8 +0,0 @@
-import warnings
-warnings.filterwarnings("ignore")
-
-from src.fetch_historical import fetch_and_store_all
-
-print("🌍 Starting historical data fetch...")
-print("This will take 20-30 minutes...\n")
-fetch_and_store_all(start_year=2015)
